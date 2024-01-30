20e1a638e6a6f8fa2437220cc30713e2d88951dc
seokcheon <my980519@gmail.com>
2024-01-30 13:02:11 +0900

    .

---
+++ docs/quests/DDA/SpineSurgeryList_apply.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+data_SpineSurgeryList =  pd.read_csv("./csv/SpineSurgeryList.csv")  # csv 읽기
+df_SpineSurgeryList = pd.DataFrame(data_SpineSurgeryList)           # csv파일을 dataframe화
+df_SpineSurgeryList.info()                                          # df 정보 읽기
+df_SpineSurgeryList[["체중","신장"]]                                 # df 내 해당 컬럼 확인
+
+def BMI(params):
+    weight = params.loc["체중"]
+    tall = params.loc["신장"]*0.01		
+    result = weight/tall**2
+    return result
+df_SpineSurgeryList["BMI"] = df_SpineSurgeryList[["체중", "신장"]].apply(BMI, axis=1)
+
+print(df_SpineSurgeryList["BMI"])
